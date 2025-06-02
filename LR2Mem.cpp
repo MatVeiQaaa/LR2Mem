2798f38cfccdc7f96cc8c08fe0e4e28acc1c49e6
mtvqaaa <matthehard@gmail.com>
2025-06-02 19:05:57 +0200

    fix missing sqlite ptr dereference

--- LR2Mem.cpp
+++ LR2Mem.cpp
@@ -6,7 +6,7 @@ static SafetyHookMid hook;
 static void OnMainLoop(SafetyHookContext& regs) {
 	if (LR2::isInit) return;
 	LR2::stackOffset = regs.esp;
-	LR2::pSqlite = (void*)(LR2::stackOffset + 0x34);
+	LR2::pSqlite = *(void**)(LR2::stackOffset + 0x34);
 	LR2::pGame = (LR2::game*)(LR2::stackOffset + 0xB8);
 	LR2::isInit = true;
 }
