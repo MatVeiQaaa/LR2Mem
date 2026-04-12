MatVeiQaaa/LR2Mem

commit 6a6b5f673404a503944da1503033293d17a31cf6

fix: fix includes
<windows.h> and <vfw.h> are supposed to be lower-case, this is required
for cross-compilation.
cstdint was unused.

LR2Typedefs.hpp
@@ -1,6 +1,8 @@
 #pragma once
-#include <Windows.h>
-#include <cstdint>
+#include "FMODex/fmod.h"
+#include <vfw.h>
+#include <windows.h>
+#include <winsock.h>
 
 typedef unsigned char undefined;
 typedef unsigned int undefined4;
@@ -883,7 +885,6 @@ namespace LR2 {
 	};
 
 	// RECORDING
-#include <Vfw.h>
 	struct RECORDING {
 		HDC srcHDC; /* struct_entry */
 		undefined4 unk4;
@@ -907,7 +908,6 @@ namespace LR2 {
 	};
 
 	// AUDIO
-#include "FMODex/fmod.h"
 	struct RAWSOUND {
 		int channels;
 		int samples;
@@ -1089,7 +1089,6 @@ namespace LR2 {
 		int rseed;
 		CSTR ghost; /* struct to? */
 	};
-#include <winsock.h>
 	struct NETWORK {
 		WSADATA wsa;
 		int isOnline;

commit 0620572c887b9f86a0d18c28ade5e7c8be3627c9

fix: fix fixes includes :monkey_face:
I cleaned-up includes only before committing so it wasn't tested. I made
sure it compiles now.

Fixes: 6a6b5f673404 ("fix: fix includes")

LR2Bindings.hpp
@@ -1,5 +1,8 @@
 #pragma once
+
 #include "LR2Typedefs.hpp"
+
+#include <cstdint>
 
 namespace LR2 {
 	inline bool isInit = false;

LR2Typedefs.hpp
@@ -1,7 +1,9 @@
 #pragma once
+
+#include <windows.h>
+
 #include "FMODex/fmod.h"
 #include <vfw.h>
-#include <windows.h>
 #include <winsock.h>
 
 typedef unsigned char undefined;